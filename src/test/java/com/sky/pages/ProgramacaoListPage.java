3ec59c294153041b6b159b774a8ec6c8b14e8e01
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-02-21 11:51:46 -0300

    Created explicit wait and increased implicit wait to match website's performance

--- src/test/java/com/sky/pages/ProgramacaoListPage.java
+++ src/test/java/com/sky/pages/ProgramacaoListPage.java
@@ -1,5 +1,6 @@
 package com.sky.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +8,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProgramacaoListPage {
     private WebDriver driver;
@@ -47,6 +50,9 @@ public class ProgramacaoListPage {
     }
 
     public void getInformationDetailScreen(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.className("sky-modal-program-description")));
         showTitleDetailScreen = programmeTitleDetailScreen.getText();
         showDurationDetailScreen = programmeDurationDetailScreen.getText();
     }
--- src/test/java/com/sky/tests/CheckCurrentlyAiringProgrammeTest.java
+++ src/test/java/com/sky/tests/CheckCurrentlyAiringProgrammeTest.java
@@ -22,7 +22,7 @@ public class CheckCurrentlyAiringProgrammeTest {
     public void setup(){
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @AfterEach
