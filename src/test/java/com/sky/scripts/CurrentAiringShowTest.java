de8ee9072445a05af7eb05f09c186ced37f05e9c
Rafael Bandeira dos Santos <rafael.bandeiradossantos-ext@ab-inbev.com>
2021-07-01 01:05:41 -0300

    refactor dashboard

---
+++ src/test/java/com/sky/scripts/CurrentAiringShowTest.java
@@ -0,0 +1,30 @@
+package com.sky.scripts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CurrentAiringShowTest {
+    @BeforeEach
+    public void setup() {
+        // instantiate the browser
+    }
+
+    @AfterEach
+    public void teardown() {
+        // close the damn browser
+    }
+
+    @Test
+    @DisplayName("Verify the currently airing show")
+    public void testVerifyTheCurrentlyAiringShow() {
+        // close the welcome screen
+
+        // navigate via dashboard to programme page
+
+        // scroll to programme list
+
+        // open the currently airing page
+    }
+}
